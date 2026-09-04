8db01887699a9c380e134c3f9301412e5677da10
gssousadefreitas2008-bit <gssousadefreitas2008@gmail.com>
2026-09-04 07:54:25 -0300

    adiel

--- Adiel.Poo.py
+++ Adiel.Poo.py
@@ -24,5 +24,4 @@ pessoa1.apresentacao()
 estudante1 = Estudante("Adiel", 17, "2º")
 estudante1.apresentacao()
 estudante1.grade()
-estudante2 = Estudante("João", 16, "1º")
-
+estudante2 = Estudante("João", 16, "1º"
